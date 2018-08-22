1455200f962b280b88021c57257cc7434e7c04bd
Seb Gertz <sebgertz@gmail.com>
2018-08-22 02:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // set up basic form
 function initForm() {
+  // 1. write query to store HTML element 
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
